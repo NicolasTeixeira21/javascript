9c61870022b2994a8eeaacfbc348fc2ee20fcf9c
Nicolas Teixeira <104282872+NicolasTeixeira21@users.noreply.github.com>
2023-02-21 19:54:15 -0300

    atualizado

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // condição simples!
+    console.log('Você ultapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija com segurança!')
