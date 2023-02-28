167955ce6c2f0e1a77d9a25b1f55261a4f45acda
Nicolas Teixeira <104282872+NicolasTeixeira21@users.noreply.github.com>
2023-02-28 20:25:45 -0300

    atualizado

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,12 +10,41 @@ function verificar() {
         var idade = ano - fano.value
         //res.innerHTML = ` Você tem ${idade}`
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'criancamas.png')
+            } else if (idade >= 10 && idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'jovemmas.png')
+            } else if (idade >= 21 && idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adultomas.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'homemidoso.png')
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if ( idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'criancafem.png')
+            } else if (idade >=10 && idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'jovemfem.png')
+            } else if (idade >= 21 && idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adultofem.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'mulheridosa.png')
+            }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
         res.style.textAlign = 'center'
+        res.appendChild(img)
     }
 }
